99c23013b28916c032540fe7fc0c827e5a053151
ali <ali.mozafari733@yahoo.com>
2023-08-08 23:38:36 +0430

    add addEventListener js

--- shomarande.js
+++ shomarande.js
@@ -1,3 +1,12 @@
 
 const input = document.querySelector(".field-input");
 const counter = document.querySelector(".field-counter");
+const maxlength = input.getAttribute("maxlength");
+counter.innerHTML = maxlength;
+
+input.addEventListener("input", (e) => {
+  const valuelength = e.target.value.length;
+  const leftcharlength = maxlength - valuelength;
+  if (leftcharlength < 0) return;
+  counter.innerHTML = leftcharlength;
+});
